29a7fd0a7b98580ed4ea1a1e0a733c0a78871297
olivier.carpentier@gmail.com <ocarpen@microsoft.com>
2016-10-12 18:38:56 +0200

    Line Chart added

---
+++ src/webparts/lineChart/loc/mystrings.d.ts
@@ -0,0 +1,45 @@
+declare interface ILineChartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  Width: string;
+  Height: string;
+  Items: string;
+  ManageItems: string;
+  Label: string;
+  Value: string;
+  Color: string;
+  HoverColor: string;
+  OptionsGroupName: string;
+  Horizontal: string;
+  XAxesEnable: string;
+  YAxesEnable: string;
+  AxesFont: string;
+  AxesFontSize: string;
+  AxesFontColor: string;
+  TitleGroupName: string;
+  TitleEnable: string;
+  Title: string;
+  Position: string;
+  TitleFont: string;
+  TitleSize: string;
+  TitleColor: string;
+  LegendGroupName: string;
+  LegendEnable: string;
+  LegendPosition: string;
+  LegendFont: string;
+  LegendSize: string;
+  LegendColor: string;
+  Responsive: string;
+  BorderWidth: string;
+  PointStyle: string;
+  Fill: string;
+  LineTension: string;
+  ShowLine: string;
+  FillColor: string;
+  SteppedLine: string;
+}
+
+declare module 'LineChartStrings' {
+  const strings: ILineChartStrings;
+  export = strings;
+}
