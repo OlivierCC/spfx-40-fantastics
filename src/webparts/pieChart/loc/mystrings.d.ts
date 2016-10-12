594e73b09d745ac4b36955de24d2263e2ed0e0a3
olivier.carpentier@gmail.com <ocarpen@microsoft.com>
2016-10-12 10:56:28 +0200

    Pie Chart Web Part added

---
+++ src/webparts/pieChart/loc/mystrings.d.ts
@@ -0,0 +1,37 @@
+declare interface IPieChartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  Width: string;
+  Height: string;
+  Items: string;
+  ManageItems: string;
+  Label: string;
+  Value: string;
+  Color: string;
+  HoverColor: string;
+  OptionsGroupName: string;
+  CutoutPercentage: string;
+  Rotation: string;
+  Circumference: string;
+  AnimateRotate: string;
+  AnimateScale: string;
+  TitleGroupName: string;
+  TitleEnable: string;
+  Title: string;
+  Position: string;
+  TitleFont: string;
+  TitleSize: string;
+  TitleColor: string;
+  LegendGroupName: string;
+  LegendEnable: string;
+  LegendPosition: string;
+  LegendFont: string;
+  LegendSize: string;
+  LegendColor: string;
+  Responsive: string;
+}
+
+declare module 'PieChartStrings' {
+  const strings: IPieChartStrings;
+  export = strings;
+}
